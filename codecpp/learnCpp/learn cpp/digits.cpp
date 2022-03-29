fd900189c7c7fd38ae8aedfa16472ac60662a689
aky360 <95171943+aky360@users.noreply.github.com>
2022-03-29 16:29:19 +0530

    Update digits.cpp

--- codecpp/learnCpp/learn cpp/digits.cpp
+++ codecpp/learnCpp/learn cpp/digits.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 bool oneDigit(int *num){
     if(*num < 0){
-        cout<<"the number is greater than 0 "<<endl;
+        cout<<"the number is less than 0 "<<endl;
         return true;
     }
     else if(*num >= 0 && (*num)/10 > 10){
-        cout<<(*num)/10<<"the number is greater than 0 and single number "<<endl;
+        cout<<(*num)/10<<"the number is greater than 0 "<<endl;
         return true;
     }
     else{
@@ -18,10 +18,21 @@ bool oneDigit(int *num){
     return false;
 }
 
+
+int ret(int *num){
+    if(*num > 0){
+        *num = *num+1;
+    }
+    return *num;
+}
+
 // Driver program
 int main(){
     string str = "nitin";
     int a = 123;
+    cout<<"ret "<<ret(&a)<<endl;    // output 124
+    int x = 1;
+    cout<<"mod "<<x%10<<" .. 1/10 "<<1/10<<endl;    //output   mod 1 .. 1/10 0
     cout<<oneDigit(&a)<<endl;
     
     return 0;
