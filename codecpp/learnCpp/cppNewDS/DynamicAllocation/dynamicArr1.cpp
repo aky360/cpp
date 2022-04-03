b33e0a1d179da48c4dfb62c50ee3de3d7cee548e
aky360 <95171943+aky360@users.noreply.github.com>
2022-04-03 13:02:54 +0530

    Update dynamicArr1.cpp

--- codecpp/learnCpp/cppNewDS/DynamicAllocation/dynamicArr1.cpp
+++ codecpp/learnCpp/cppNewDS/DynamicAllocation/dynamicArr1.cpp
@@ -1,1 +1,43 @@
+#include<iostream>
+using namespace std;
 
+int getSum(int *arr, int n) {
+
+    int sum = 0;
+    for(int i=0; i<n; i++) {
+        sum += arr[i];
+    } 
+    return sum;
+}
+
+
+int main() {
+
+    int n;
+    cin >> n;
+    
+    //variable size array  OR DYNAMIC ARRAY CREATIONS IN HEAP MEMORY
+    int* arr = new int[n];
+
+    //takign inputn in aray
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int ans = getSum(arr, n);
+
+    cout << "answer is "<< ans << endl;
+
+//case 1 where it creates infinite, new int type space in stack memory at every iteration UNTIL THE STACK OVERFLOWS.
+    while(true) {
+        int i = 5;
+    }
+
+    
+//case 2 where it creates infinite, new dynamic allocation memory at every iteraton in heap until the heap overflows.
+    while(true) {
+        int* ptr = new int;
+    }
+
+    return 0;
+}
