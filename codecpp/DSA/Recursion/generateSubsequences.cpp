cc9973dc55be88a3c36e8f57d5854138790f076a
aky360 <95171943+aky360@users.noreply.github.com>
2022-04-04 15:34:39 +0530

    Update generateSubsequences.cpp

--- codecpp/DSA/Recursion/generateSubsequences.cpp
+++ codecpp/DSA/Recursion/generateSubsequences.cpp
@@ -1,93 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 class Solution{
 public:
-	void solve(vector<string> str, string output, int index, vector<string>& ans){
-
-		if(index >= str.size()){
+	void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans){
+		//base case
+		if(index >= nums.size()){
 			ans.push_back(output);
 			return ;
 		}
+		//exclude the item
+		solve(nums, output, index+1, ans);
 
-		//exclude
-		solve(str, output, index+1, ans);
-
-		//include
-		output = output + str[index];
-		solve(str, output, index+1, ans);
+		//include the item
+		int element = nums[index];
+		output.push_back(element);
+		solve(nums, output, index+1, ans);
 	}
 
-	vector<string> subsequences(vector<string>& nums){
-
-		vector<string> ans;
-		string output = "";
+	vector<vector<int>> subsets(vector<int>& nums){
+		vector<vector<int>> ans;
+		vector<int> output;
 		int index = 0;
 		solve(nums, output, index, ans);
-
 		return ans;
-	}
-}; 
-
-
-class SolutionLB{
-public:
-	void solve(vector<string>& ans, string str, string output, int i) {
-    	//base case
-    	if(i>=str.length()) {
-        	if(output.length()>0)
-        		ans.push_back(output);
-        	return ;
-        }
-
-    	//exclude
-    	solve(ans, str, output, i+1);
-
-    	//include
-    	output.push_back(str[i]);
-    	solve(ans, str, output, i+1);
-	}
-
-	vector<string> subsequences(string str){
-	
-		vector<string> ans;
-    	string output = "";
-    	solve(ans,str,output,0);
-    	
-    	return ans;
 	}
 };
 
-
-
-void printVector(vector<string> vec){
-	for (vector<string>::iterator it = vec.begin(); it != vec.end(); ++it){
-        cout << *it<< endl;
+void printNestedVector1(vector<vector<int>> vec){
+	for (vector<vector<int>>::iterator it = vec.begin(); it != vec.end(); ++it){
+        for (vector<int>::iterator it1 = (*it).begin(); it1 != (*it).end(); ++it1){
+            cout << *it1 << " ";
+        }
+        cout << endl;
     }
 }
 
+void printNestedVector2(vector<vector<int>> vec){
+	for (const auto& v : vec){
+    	for (auto i : v){
+        	cout << i << " ";
+    	}
+    	cout << endl;
+	}
+}
+
 int main(){
-	vector<string> str = {"a", "b", "c"};
-	string str1 = "abc";
-
+	vector<int> nums = {1,2,3};
 	Solution sol;
-	SolutionLB solLB;
-
-	//cout<<"generate subsequences for the str values "<<endl;
-
-	vector<string> vec = sol.subsequences(str);
-	cout<<"subsequences solution 1 with vec "<<endl;
-	printVector(vec);
-	cout<<endl;
-
-	vector<string> vecs = solLB.subsequences(str1);
-	cout<<"subsequences solution 2 with str "<<endl;
-	printVector(vecs);
-	cout<<endl;
-
-	cout <<"original values "<< endl;
-	printVector(str);
-
+	cout<<"generate subsets for the nums values "<<endl;
+	vector<vector<int>> vec = sol.subsets(nums);
+	printNestedVector1(vec);
+	cout << endl;
+	printNestedVector2(vec);
 	return 0; 
 }
