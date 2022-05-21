34f1e78e7f96072a7c822b30437385b36d8977b6
aky360 <95171943+aky360@users.noreply.github.com>
2022-05-21 13:21:20 +0530

    Create strConNumUpLo.cpp

---
+++ codecpp/learnCpp/cppNewDS/String/strConNumUpLo.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <algorithm>
+#include <cctype>
+#include <string>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+bool has_any_digits(const string& s)
+{
+    return any_of(s.begin(), s.end(), ::isdigit);
+}
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin>>str;
+	    int upper = 0, lower = 0, number = 0;
+	    for(string::size_type i=0;i<str.size();i++){
+            if (str[i] >= 'A' && str[i] <= 'Z'){
+                upper++;
+            }
+            else if (str[i] >= 'a' && str[i] <= 'z'){
+                lower++;
+	        }
+            else (str[i]>= '0' && str[i]<= '9'){
+                number++;
+	        }
+        }
+        if(upper > 0 && lower > 0 && number > 0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        } 
+	}
+	return 0;
+}
