3ad6fd3f3d859b1b9c83a63f4c5ca51304d7fa93
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-01 18:08:48 +0530

    Update binarySearchRecr.cpp

--- codecpp/DSA/Recursion/binarySearchRecr.cpp
+++ codecpp/DSA/Recursion/binarySearchRecr.cpp
@@ -18,12 +18,37 @@ int binarySearch(int *arr, int k, int s, int e){
     }
     return -1;
 }
+//Time Complexity: O(log n)
+//Auxiliary Space: O(log n)
+
 
 void printArr(int *arr, int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
 }
+
+
+
+//Iterative implementation of Binary Search 
+int binarySearch(int *arr, int x,  int l, int r){
+    while(l<=r){
+        int m = l + (r - l) / 2;
+        if(arr[m] == x){
+            return m;
+        }
+        if(arr[m] < x){
+            l = m + 1;
+        }
+        else{
+            r = m - 1;
+        }
+    }
+    return -1;
+}
+//Time Complexity: O(log n)
+//Auxiliary Space: O(1)
+
 
 
 int main() {
@@ -37,7 +62,3 @@ int main() {
 
     return 0;
 }
-
-
-//Time Complexity: O(log n)
-//Auxiliary Space: O(log n)
