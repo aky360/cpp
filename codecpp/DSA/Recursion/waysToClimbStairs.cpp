035d128d2f5b7b6ef9969c823ce3703bfc2073b1
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-01 12:21:41 +0530

    Create waysToClimbStairs.cpp

---
+++ codecpp/DSA/Recursion/waysToClimbStairs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+
+
+int solve(long long nStairs, int i){
+    if(i == nStairs)
+        return 1;
+    if(i>nStairs)
+        return 0;
+    
+    return solve(nStairs, i+1) + solve(nStairs, i+2) % MOD;
+}
+
+
+int countDistinctWayToClimbStair(long long nStairs){
+    int ans = solve(nStairs, 0);
+    return ans;
+}
+
+
+int main() {
+
+    return 0;
+}
