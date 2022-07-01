7aaca05aee59c080a6a5ea52a6cb4e607b71fef3
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-01 18:18:24 +0530

    Create revStr.cpp

---
+++ codecpp/DSA/Recursion/revStr.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+
+
+void reverseStr(string &str, int i, int j){
+    if(i>j){
+        return ;
+    }
+    swap(str[i], str[j]);
+    i++;
+    j--;
+    return reverseStr(str, i, j);
+}
+
+
+int main() {
+
+    string name = "abcde";
+    cout << endl;
+    reverse(name, 0 , name.length()-1 );
+    cout << endl;
+    cout << name << endl;
+
+    return 0;
+}
