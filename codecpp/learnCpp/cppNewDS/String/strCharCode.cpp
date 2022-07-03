3690b080cd569ec498d4217f8cc4fd2302af2c51
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-04 00:56:57 +0530

    Create strCharCode.cpp

---
+++ codecpp/learnCpp/cppNewDS/String/strCharCode.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+
+//Upper to Lower ===>> (character-'A'+'a')
+//Lower to Upper ===>> (character-'a'+'A')
+// Number     === >>> ch-'0'
+
+char toLowerCase(char ch){
+	if(ch>='a' && ch<='z')
+		return ch;
+	else{
+		char temp = ch-'A'+'a';
+		return temp;
+	}
+}
+
+// bool checkPalindrom(string s){
+// 	int s = 0;
+// 	int e = s.size()-1;
+
+// }
+
+bool checkPalindrome(char a[], int n){
+	int s=0;
+	int e=n-1;
+
+	while(s<e){
+		if(toLowerCase(a[s]) != toLowerCase(a[e])){
+			return 0;
+		}
+		else{
+			s++;
+			e--;
+		}
+	}
+	return 1;
+}
+
+void reverseStr(char name[], int n){
+	int s=0; 
+	int e = n-1; 
+	while(s<e){
+		swap(name[s++], name[e--]);
+	}
+}
+
+int getLength(char name[]){
+	int count = 0;
+	for (int i = 0;name[i] !='\0';i++){
+		count++;
+	}
+	return count;
+}
+
+
+int main(){
+	char name[20];
+
+	cout<<"Enter your name "<<endl;
+	cin>>name;
+
+	//name[2] = '\0';
+
+	cout<<"your name is ";
+	cout<<name<<endl;
+
+	int len = getLength(name);
+
+	cout<<"Length "<<len<<endl;
+	reverseStr(name, len);
+	cout<<"Your name is "<<name<<endl;
+
+	cout<<"checkPalindrome or not "<<checkPalindrome(name, len)<<endl;
+
+	return 0;
+}
