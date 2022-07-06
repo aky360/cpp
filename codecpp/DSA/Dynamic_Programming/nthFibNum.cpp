7b127162cb08da2df05fe81125aee1697b7befe2
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-06 16:22:33 +0530

    Update nthFibNum.cpp

--- codecpp/DSA/Dynamic_Programming/nthFibNum.cpp
+++ codecpp/DSA/Dynamic_Programming/nthFibNum.cpp
@@ -1,9 +1,13 @@
 //Nth Fibonacci Number code studio coding Ninja
+//Top Down = Recursion + Memoization
+//Bottom Up = Tabulation
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int fib(int n, vector<int> &dp){
+
+//This solution is using Recursion + Memoization method
+int fibMemoization(int n, vector<int> &dp){
     if(n<=1){
         return n;
     }
@@ -11,9 +15,27 @@ int fib(int n, vector<int> &dp){
         return dp[n];
     } 
     
-    dp[n] = fib(n-1, dp)+fib(n-2, dp);
+    dp[n] = fibMemoization(n-1, dp)+fibMemoization(n-2, dp);
     return dp[n];
 }
+//T.C. = O(n)
+//S.C. = O(n) + O(n) = O(n);
+
+
+//This solution is using the Tabulation method 
+void fibTabulation(int n){
+    vector<int> dp(n+1);     //vector of size n+1;
+    dp[0] = 0;               //initial values
+    dp[1] = 1;
+    
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    
+    cout<<dp[n]<<endl;
+}
+//T.C. = O(n)
+//S.C. = O(n)
 
 int main(){
     int n;
@@ -24,7 +46,7 @@ int main(){
     for(int i=0;i<=n;i++)
         dp[i] = -1;
         
-    cout<<fib(n, dp)<<endl;
-    
+    cout<<fibMemoization(n, dp)<<endl;
+    fibTabulation(n);
     return 0;
 }
