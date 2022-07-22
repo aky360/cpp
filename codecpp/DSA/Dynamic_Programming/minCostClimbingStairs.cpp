ca66fdff15779314c5fd42f8a56f1549695071df
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-22 16:34:40 +0530

    Create minCostClimbingStairs.cpp

---
+++ codecpp/DSA/Dynamic_Programming/minCostClimbingStairs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> &cost, int n){
+    if(n == 0)
+        return cost[0];
+
+    if(n==1)
+        return cost[1];
+        
+    return cost[n] + min(solve(cost, n-1), solve(cost, n-2));
+}
+
+
+int minCostClimbingStairs(vector<int> &cost){
+    int n = cost.size();
+    return min(solve(cost, n-1), solve(cost, n-2));
+}
+
+
+int main() {
+  
+    vector<int> vec = {10, 15, 20};
+    
+    cout<<minCostClimbingStairs(vec)<<endl;
+
+    return 0;
+}
