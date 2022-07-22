7c36900d64dd97f859dba1273d2938b5aa04d781
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-22 18:18:40 +0530

    Update minCostClimbingStairs.cpp

--- codecpp/DSA/Dynamic_Programming/minCostClimbingStairs.cpp
+++ codecpp/DSA/Dynamic_Programming/minCostClimbingStairs.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//solving using simple recursion method
 int solve(vector<int> &cost, int n){
     if(n == 0)
         return cost[0];
-
     if(n==1)
         return cost[1];
         
@@ -15,6 +15,27 @@ int solve(vector<int> &cost, int n){
 int minCostClimbingStairs(vector<int> &cost){
     int n = cost.size();
     return min(solve(cost, n-1), solve(cost, n-2));
+}
+
+
+//solving using Top Down(Recursion + Memoization) methods
+int solve(vector<int> &cost, int n, vector<int> &dp){
+    if(n == 0)
+        return cost[0];
+    if(n==1)
+        return cost[1];
+    if(dp[n] != -1){
+        return dp[n];
+    }
+        
+    dp[n] = cost[n] + min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+    return dp[n];
+}
+
+int minCostClimbingStairs(vector<int> &cost){
+    int n = cost.size();
+    vector<int> dp(n+1, -1);
+    return min(solve(cost, n-1, dp), solve(cost, n-2, dp));
 }
 
 
