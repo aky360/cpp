0e54b0e9f6022230cfcb12201bc8161678df4403
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-27 15:40:59 +0530

    Update minimumCoin.cpp

--- codecpp/DSA/Dynamic_Programming/minimumCoin.cpp
+++ codecpp/DSA/Dynamic_Programming/minimumCoin.cpp
@@ -22,10 +22,42 @@ int solve(vector<int> &num, int x){
     return mini;
 }
 
+
+//using MEMOIZATION DP TECHNIQUE 
+int solveMemoization(vector<int> &num, int x, vector<int> &dp){
+    if(x==0){
+        return 0;
+    }
+    if(x<0){
+        return INT_MAX;
+    }
+    if(dp[x] != -1){
+        return dp[x];
+    }
+    
+    int mini = INT_MAX;
+    for(int i=0;i<num.size();i++){
+        int ans = solve(num, x-num[i]);
+        if(ans != INT_MAX){
+            mini = min(mini, 1+ans);
+        }
+    }
+    dp[x] = mini;
+    return dp[x];
+}
+
+
 int minimumElements(vector<int> &num, int x){
-    int ans = solve(num, x);
+    vector<int> dp(x+1, -1);
+    int ans  = solveMemoization(num, x, dp);
+    
+    //int ans = solve(num, x);
+    
     if(ans == INT_MAX){
         return -1;
+    }
+    else{
+        return ans;
     }
     return ans;
 }
