9833e9f01c224df88f38ace9d9d4506f5071a227
aky360 <95171943+aky360@users.noreply.github.com>
2022-08-03 10:50:22 +0530

    Create postOrderRec.cpp

---
+++ codecpp/DSA/Trees/treeTraversals/postOrderRec.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void postOrder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+    
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+
+int main() {
+    Node* NewRoot = NULL;
+    Node* root = new Node(2);
+    root->left = new Node(7);
+    root->right = new Node(5);
+    root->left->right = new Node(6);
+    root->left->right->left = new Node(1);
+    root->left->right->right = new Node(11);
+    root->right->right = new Node(9);
+    root->right->right->left = new Node(4);
+    root->right->right->left->right = new Node(8);
+//          2
+//        /  \
+//       7    5
+//        \    \
+//        6     9 
+//       / \   /
+//      1  11 4
+//             \
+//              8
+    postOrder(root);
+    cout<<endl;
+    
+    return 0;
+}
